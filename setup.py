cf295be54973d50e4ff1f5707988dac05be0ed32
uibkaccsev <severin.roessler@student.uibk.ac.at>
2025-07-21 08:58:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ VERSION = '1.0.5'
 
 setup(
     name='pyvesc',
-    packages=['pyvesc', 'pyvesc.protocol', 'pyvesc.protocol.packet', 'pyvesc.VESC.messages'],
-    modules=['pyvesc.VESC.py'],
+    packages=['pyvesc', 'pyvesc.protocol', 'pyvesc.protocol.packet', 'pyvesc.messages', 'pyvesc.params'],
     version=VERSION,
     description='Python implementation of the VESC communication protocol.',
     author='Liam Bindle',
